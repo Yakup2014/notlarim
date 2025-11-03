b70dd6cbf9fbe7c99f12a2eb0bfb95405a1d191a
Yakup Efe <senyakupefe07@gmail.com>
2025-11-03 21:30:45 +0300

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include<cstring>
+using namespace std;
+int main()
+{
+    ofstream dosya("deneyap.txt");
+    
+   string konsoldanOkunan;
+   while(konsoldanOkunan!="çık"){
+       getline(cin,konsoldanOkunan);
+       dosya<<konsoldanOkunan<<endl;
+   }
+   dosya.close();
+
+    return 0;
+}
